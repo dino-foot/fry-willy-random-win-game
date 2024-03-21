2f2dcc378966d396338f28ee2ab538b84912bf98
Shohanur Rahaman <shohan4556@gmail.com>
2024-03-21 14:23:49 +0600

    gameover scene done

--- src/scenes/GameOver.ts
+++ src/scenes/GameOver.ts
@@ -1,35 +1,41 @@
-import { Scene } from 'phaser';
+import { Scene, Display, GameObjects } from 'phaser';
 
-export class GameOver extends Scene
-{
+export class GameOver extends Scene {
     camera: Phaser.Cameras.Scene2D.Camera;
     background: Phaser.GameObjects.Image;
-    gameover_text : Phaser.GameObjects.Text;
+    logo: Phaser.GameObjects.Image;
+    playAgain: GameObjects.Image;
+    youWin: GameObjects.Image;
 
-    constructor ()
-    {
+    constructor() {
         super('GameOver');
     }
 
-    create ()
-    {
+    create() {
+        const gameWidth = Number(this.game.config.width);
+        const gameHeight = Number(this.game.config.height);
+
         this.camera = this.cameras.main
-        this.camera.setBackgroundColor(0xff0000);
+        this.background = this.add.image(this.camera.centerX, this.camera.centerY, 'sky_desktop');
+        this.background.setOrigin(0.5);
 
-        this.background = this.add.image(512, 384, 'background');
-        this.background.setAlpha(0.5);
+        this.logo = this.add.image(0, 0, 'logo');
+        this.logo.setScale(0.5).setDepth(2);
+        Display.Align.In.TopCenter(this.logo, this.add.zone(this.camera.centerX, this.camera.centerY, gameWidth, gameHeight), 0, 200);
 
-        this.gameover_text = this.add.text(512, 384, 'Game Over', {
-            fontFamily: 'Arial Black', fontSize: 64, color: '#ffffff',
-            stroke: '#000000', strokeThickness: 8,
-            align: 'center'
-        });
-        this.gameover_text.setOrigin(0.5);
+        this.playAgain = this.add.image(0, 0, 'play-again');
+        this.playAgain.setScale(0.8).setDepth(1);
+        this.playAgain.setInteractive();
 
-        this.input.once('pointerdown', () => {
-
+        this.playAgain.on('pointerdown', () => {
+            console.log('restart');
             this.scene.start('MainMenu');
-
         });
+
+        Display.Align.In.BottomCenter(this.playAgain, this.logo, 0, 250);
+
+        this.youWin = this.add.image(0, 0, 'you-win');
+        this.youWin.setScale(0.5).setDepth(1);
+        Display.Align.In.BottomCenter(this.youWin, this.logo, 50, 200);
     }
 }
